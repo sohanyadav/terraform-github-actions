01a012125fd490def66683440d66ccfe168f974a
Sohan Yadav <sohan@clouddrove.com>
2019-12-06 21:44:30 +0530

    initial commit

--- src/main.sh
+++ src/main.sh
@@ -84,16 +84,22 @@ function installTerraform {
 }
 
 function installGo {
+  echo "Install Go for terratest"
   wget -q -O - https://raw.githubusercontent.com/canha/golang-tools-install-script/master/goinstall.sh \
  | bash -s -- --version 1.13.2
+  echo "Install Go package fo terratest"
   go get github.com/gruntwork-io/terratest/modules/terraform github.com/stretchr/testify/assert
 }
 
 function InsallKuguard {
+    echo "Install Go for kuguard"
     wget -q -O - https://raw.githubusercontent.com/canha/golang-tools-install-script/master/goinstall.sh \
  | bash -s -- --version 1.12.12
-  cp ../kuguard -r $HOME/go/src
-  cd $HOME/go/src  && go install kuguard
+    echo "Install Go for kuguard"
+    echo $HOME
+    cp ../kuguard -r $HOME/go/src
+    echo "Install kuguard"
+    cd $HOME/go/src  && go install kuguard
 }
 
 

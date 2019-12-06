dbc6e77c41e1b6ba5efaa1086230f210c11c3a2d
Sohan Yadav <sohan@clouddrove.com>
2019-12-06 11:31:02 +0530

    initial commit

--- src/main.sh
+++ src/main.sh
@@ -85,7 +85,9 @@ function installTerraform {
 function installGo {
   wget -q -O - https://raw.githubusercontent.com/canha/golang-tools-install-script/master/goinstall.sh \
  | bash -s -- --version 1.13.2
-  go get github.com/gruntwork-io/terratest/modules/terraform
+  go get github.com/gruntwork-io/terratest/modules/terraform github.com/stretchr/testify/assert
+
+
 }
 
 function main {

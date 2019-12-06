3e74ed7af0b185064bbcf37b42e48c3db9d8a1ae
Sohan Yadav <sohan@clouddrove.com>
2019-12-06 12:50:22 +0530

    initial commit

--- src/main.sh
+++ src/main.sh
@@ -82,11 +82,11 @@ function installTerraform {
   fi
   echo "Successfully unzipped Terraform v${tfVersion}"
 }
+
 function installGo {
   wget -q -O - https://raw.githubusercontent.com/canha/golang-tools-install-script/master/goinstall.sh \
  | bash -s -- --version 1.13.2
   go get github.com/gruntwork-io/terratest/modules/terraform github.com/stretchr/testify/assert
-
 }
 
 function main {
@@ -98,7 +98,7 @@ function main {
   source ${scriptDir}/terraform_plan.sh
   source ${scriptDir}/terraform_apply.sh
   source ${scriptDir}/terraform_output.sh
-  source ${scriptDir}/go.sh
+  source ${scriptDir}/terratest_go.sh
 
   parseInputs
   configureCLICredentials

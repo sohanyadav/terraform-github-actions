55cf12f4b01a1c3332b453f176821f344540f89d
Sohan Yadav <sohan@clouddrove.com>
2019-12-09 17:21:15 +0530

    initial commit

--- src/main.sh
+++ src/main.sh
@@ -92,27 +92,6 @@ function installGo {
 }
 
 
-function builBinary {
-    echo "Install Go for kuguard"
-    wget -q -O - https://raw.githubusercontent.com/canha/golang-tools-install-script/master/goinstall.sh \
- | bash -s -- --version 1.13.2
-    apk add  zip
-    echo "copy  kuguard"
-    cp kuguard -r $HOME/go/src
-    echo "Install go  package"
-    go get github.com/mitchellh/go-homedir github.com/spf13/cobra
-    echo  "Build binary for Mac"
-    env GOOS=darwin GOACRCH=amd64 go build -o kuguard_darwin_amd64/kuguard kuguard
-    echo  "Zip the Mac binary file"
-    zip -r kuguard_darwin_amd64.zip kuguard_darwin_amd64/kuguard
-
-    echo "Build binary for Linux"
-    env GOOS=linux GOACRCH=amd64 go build -o kuguard_linux_amd64/kuguard kuguard
-    echo  "Zip the Linux binary file"
-    zip -r kuguard_linux_amd64.zip kuguard_linux_amd64/kuguard
-
-}
-
 function main {
   # Source the other files to gain access to their functions
   scriptDir=$(dirname ${0})

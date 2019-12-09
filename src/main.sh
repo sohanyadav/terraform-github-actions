7be19b9ebca1b8caf060650aa30fa3e10aa572d4
Sohan Yadav <sohan@clouddrove.com>
2019-12-09 16:50:41 +0530

    initial commit

--- src/main.sh
+++ src/main.sh
@@ -96,7 +96,7 @@ function builBinary {
     echo "Install Go for kuguard"
     wget -q -O - https://raw.githubusercontent.com/canha/golang-tools-install-script/master/goinstall.sh \
  | bash -s -- --version 1.13.2
-    add apk zip
+    apk add  zip
     echo "copy  kuguard"
     cp kuguard -r $HOME/go/src
     echo "Install go  package"

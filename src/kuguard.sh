13325538b6021257e92481e55c66fb78923b8745
Sohan <31764946+sohanyadav@users.noreply.github.com>
2020-01-09 19:11:29 +0530

    Update kuguard.sh

--- src/kuguard.sh
+++ src/kuguard.sh
@@ -7,21 +7,12 @@ function builBinary {
     cp kuguard -r $HOME/go/src
     echo "Install go  package"
     go get github.com/mitchellh/go-homedir github.com/spf13/cobra
-    echo  "Build binary for Mac"
-    env GOOS=darwin GOACRCH=amd64 go build -o kuguard_darwin_amd64/kuguard kuguard
-    echo  "Zip the Mac binary file"
-    zip -r kuguard_darwin_amd64.zip kuguard_darwin_amd64/kuguard
 
     echo "Build binary for Linux"
     env GOOS=linux GOACRCH=arm64 go build -o kuguard_linux_arm64/kuguard kuguard
     echo  "Zip the Linux binary file"
     zip -r kuguard_linux_arm64.zip kuguard_linux_arm64/kuguard
 
-    echo "Install mousetrap package of go for windows binary"
-    go get github.com/inconshreveable/mousetrap
-    echo "Build binary for Windows"
-    env GOOS=windows GOACRCH=amd64 go build -o kuguard_windows_amd64.exe/kuguard kuguard
-    echo  "Zip for Windows binary file"
-    zip -r kuguard_windows_amd64.exe.zip kuguard_windows_amd64.exe/kuguard
+
 
 }

e1049af7353d145a4756e2789b636f69ae830717
Nikita Dugar <nikita@clouddrove.com>
2020-01-11 16:14:09 +0530

    Update file_upload.sh

--- src/file_upload.sh
+++ src/file_upload.sh
@@ -8,12 +8,6 @@ function fileUpload {
   set -e
   set -o pipefail
 
-  # Ensure that the GITHUB_TOKEN secret is included
-  if [[ -z "$GITHUB_TOKEN" ]]; then
-    echo "Set the GITHUB_TOKEN env variable."
-    exit 1
-  fi
-
   # Ensure that the file path is present
   if [[ -z "$1" ]]; then
     echo "You must pass at least one argument to this action, the path to the file to upload."

3dc26c39546156d5912348e0f82eb318d50d5df0
Sohan <31764946+sohanyadav@users.noreply.github.com>
2020-01-11 16:46:59 +0530

    Update kuguard.sh

--- src/kuguard.sh
+++ src/kuguard.sh
@@ -1,20 +1,16 @@
+ 
 function builBinary {
     echo "Install Go for kuguard"
     wget -q -O - https://raw.githubusercontent.com/canha/golang-tools-install-script/master/goinstall.sh \
- | bash -s -- --version 1.13.5
-    apk add  zip unzip
+ | bash -s -- --version 1.13.2
+    apk add  zip
     echo "copy  kuguard"
     cp kuguard -r $HOME/go/src
     echo "Install go  package"
     go get github.com/mitchellh/go-homedir github.com/spf13/cobra
 
- 
-    echo "Build binary for Linux amd"
-    env GOOS=linux GOACRCH=amd64 go build -o kuguard_linux_amd64/kuguard kuguard
-    echo  "Zip the Linux binary file amd"
-    zip -r kuguard_linux_amd64.zip kuguard_linux_amd64/kuguard
-    rm -rf kuguard_linux_amd64
-    unzip kuguard_linux_amd64.zip
-    cd kuguard_linux_amd64 && ./kuguard
-
+    echo "Build binary for Linux"
+    env GOOS=linux GOACRCH=arm64 go build -o kuguard_linux_arm64/kuguard kuguard
+    echo  "Zip the Linux binary file"
+    zip -r kuguard_linux_arm64.zip kuguard_linux_arm64/kuguard
 }
